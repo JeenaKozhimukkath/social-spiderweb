d32534fe31385ab2ee7dafe6ee18a8e6c1670955
MadelineLowes <madeline.lowes@gmail.com>
2023-03-04 10:26:06 -0500

    added code to index.js & user model

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Thoughts = require('./Thoughts');
+const Friends = require('./Friends');
+const Messages = require('./Messages');
+
+module.exports = { User, Thoughts, Friends, Messages };
